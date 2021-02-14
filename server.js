0ff3c584a51a1ab2168573639c502c85b43b6bc8
Shawn McPhee <scmcphee9@gmail.com>
2021-02-14 13:37:36 -0700

    updated server and deploying

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
